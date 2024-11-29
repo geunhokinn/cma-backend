ce70febd7033b76688ef122aa318f24f0c0aac8b
geunhokinn <gghh1220@gmail.com>
2024-11-29 22:48:35 +0900

    Feat: 프로젝트 생성

---
+++ src/test/java/cnu/cma/CmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cnu.cma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
